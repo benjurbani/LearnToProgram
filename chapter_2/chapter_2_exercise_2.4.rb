3f262140238a064bb1fec94747d04b99f86c5b7c
benjurbani <benjurbani@gmail.com>
2015-03-20 10:21:03 -0400

    Exercise 2.4 has been successfully executed.
    I must remove all personal information from this file!

---
+++ chapter_2/chapter_2_exercise_2.4.rb
@@ -0,0 +1,11 @@
+puts "hours in a year"
+puts 24*365
+
+puts "minutes in a decade"
+puts 24*60*365*10
+
+puts "my purported age in seconds"
+puts 60*60*24*35
+
+puts Time.new.year - 1975
+puts 3600*24*(Time.new.year - 1975)
